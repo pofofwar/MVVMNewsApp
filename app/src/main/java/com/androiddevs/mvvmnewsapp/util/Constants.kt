ca295901612c6ab43794dd9c5c3d804cc4707d34
Tikhonov Nikita <pofofwar@gmail.com>
2021-12-27 17:50:05 +0300

    add basic Retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6a21c4446ccd4316bd767b780a160bc4"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
