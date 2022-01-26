e46cbc698feedfe4d14baf9824bef439a2673a2a
Nikita Tikhonov <nikita@thedelta.io>
2022-01-26 19:31:10 +0300

    add basis setup of viewModel

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -1,7 +1,6 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
-import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class ArticleFragment : Fragment(R.layout.fragment_article) {
+class ArticleFragment : FragmentWithNewsViewModel(R.layout.fragment_article) {
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,7 +1,6 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
-import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
 
-class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
+class SavedNewsFragment: FragmentWithNewsViewModel(R.layout.fragment_saved_news) {
 }
